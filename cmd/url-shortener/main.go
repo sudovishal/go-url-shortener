841cb19b802329821576e5558cfa7acdda219583
Vishal Shinde <shinde.vishal1809@gmail.com>
2024-07-13 16:31:52 +0530

    database connection

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/sudovishal/go-url-shortener/internal/db"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get environment variables
+	dbURL := os.Getenv("DB_URL")
+	dbToken := os.Getenv("DB_TOKEN")
+
+	// Connect to TursoDB
+	db, err := db.Connect(dbURL, dbToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	fmt.Println("Database Connection Established")
+
+	log.Println("Server started on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
